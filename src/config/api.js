d49d25815b7a9df7012b893cfc7c043826020be7
Carl McCabe <carl.h.mccabe@gmail.com>
2019-11-20 10:46:59 +1000

    Add the environment variable

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL: 'https://whispering-earth-93774.herokuapp.com/',
+  baseURL: process.env.EXPRESS_URI ||'https://whispering-earth-93774.herokuapp.com/',
+  // Development URL
   // baseURL: 'http://localhost:3009/',
   timeout: 10000,
   withCredentials: true
