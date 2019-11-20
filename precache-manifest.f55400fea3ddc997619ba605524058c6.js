643edd2e196dcae789a6d6c7d9620f4a53964098
Carl McCabe <carl.h.mccabe@gmail.com>
2019-11-20 11:38:49 +1000

    Updates

---
+++ precache-manifest.f55400fea3ddc997619ba605524058c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e20a404d689fdd4289bb8e0bee5b6a2",
+    "url": "/blog-client/index.html"
+  },
+  {
+    "revision": "ab4f712713542b6083fb",
+    "url": "/blog-client/static/css/2.46f0bf41.chunk.css"
+  },
+  {
+    "revision": "ab4f712713542b6083fb",
+    "url": "/blog-client/static/js/2.55db4944.chunk.js"
+  },
+  {
+    "revision": "e5c966bed5c38da5a288",
+    "url": "/blog-client/static/js/main.6a090363.chunk.js"
+  },
+  {
+    "revision": "0a2fb8f7e4203d6b14ed",
+    "url": "/blog-client/static/js/runtime-main.2689d353.js"
+  }
+]);
